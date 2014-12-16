6aced2490716c728feac8b637ea8c33c20ce68bd
mikaben89 <mika.89@hotmail.fr>
2014-12-16 11:42:08 +0100

    Jquery au max

--- js/app.js
+++ js/app.js
@@ -1,1 +1,23 @@
-// Code here
+jQuery(document).ready(function($){
+	$(".display_list" ).click(
+		function(){
+			$(this).next().toggle("blind");
+	});
+
+	$.each($(".stations_number"),
+		function(){
+			var number = $(this).parent().find(".metro_list ul li").length;
+			$(this).text( number + " stations");
+		}
+	);
+	$.each($(".directions"),
+		function(){
+			var directions = $(this).parent().find(".metro_list ul li:first").text() + " - "
+			+ $(this).parent().find(".metro_list ul li:last").text() ;
+			$(this).text( directions + " stations");
+		}
+	);
+
+	
+
+});
